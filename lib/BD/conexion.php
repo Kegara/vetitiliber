f06e5d914f5bca08f547bcc7b005d68a7cdaec64
Umbrum <hakutoishiwashi@gmail.com>
2021-07-03 14:12:07 -0500

    Arreglo de datos de conexion

--- lib/BD/conexion.php
+++ lib/BD/conexion.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Accept");
     
     // Variables de la conexion a la DB
-    $conexion = new mysqli("myreviewvl.000webhostapp.com","myReviewUs","D+Gi3+{[/+sg!>Kk","myReview");
+    $conexion = new mysqli("localhost","id17151113_myreviewus","D+Gi3+{[/+sg!>Kk","id17151113_myreview");
     
     // Comprobamos la conexion
     if($conexion->connect_errno) {
