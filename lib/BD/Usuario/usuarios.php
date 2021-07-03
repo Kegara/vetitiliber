17a9040de8af15f1695fec1ed05e75157e55fe0a
Umbrum <hakutoishiwashi@gmail.com>
2021-07-03 13:33:13 -0500

    Actualizacion de nombre de la variable que contiene la conexion

--- lib/BD/Usuario/usuarios.php
+++ lib/BD/Usuario/usuarios.php
@@ -8,13 +8,13 @@ if (isset($_POST)) {
     $contraseña = $_POST["contrasena"];
 
     $sql = "SELECT id, nombre, contrasena FROM usuario WHERE nombre='$usuario' AND contrasena='$contraseña'";
-    $query = $mysqli->query($sql);
+    $query = $conexion->query($sql);
 
 
     while ($resultado = $query->fetch_assoc()) {
         echo json_encode($resultado);
     }
 
-    mysqli_close($mysqli);
+    mysqli_close($conexion);
 }
 ?>
