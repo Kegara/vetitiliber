f19bed980f4fa64103c2f12a2802215094bf5982
Paulo Romero <player_one_117@outlook.com>
2021-07-02 21:53:59 -0500

    consulta usuario con validación POST

--- lib/BD/Usuario/usuarios.php
+++ lib/BD/Usuario/usuarios.php
@@ -1,20 +1,13 @@
 <?php
-<<<<<<< HEAD
     if($_POST){
         require "/conexion.php";
-=======
 
-        require "../conexion.php";
->>>>>>> 6e349b3bd870a053b3f4a23fcb8bef31476f0e35
 
         $usuario = $_POST["nombre"];
         $contraseña = $_POST["contrasena"];
 
-<<<<<<< HEAD
+
         $sql = "SELECT id,nombre,contrasena FROM usuario WHERE nombre='$usuario' AND contrasena='$contraseña'";
-=======
-        $sql = "SELECT id, nombre, contrasena FROM usuario WHERE nombre='$usuario' AND contrasena='$contraseña'";
->>>>>>> 6e349b3bd870a053b3f4a23fcb8bef31476f0e35
         $query = $mysqli->query($sql);
         
         
