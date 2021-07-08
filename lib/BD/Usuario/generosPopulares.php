f7853f4ae1b143ed9ce41dd02ce8f3f9ea7f2f78
Umbrum <hakutoishiwashi@gmail.com>
2021-07-07 19:24:32 -0500

    php para consultas 5 generos mas populares

---
+++ lib/BD/Usuario/generosPopulares.php
@@ -0,0 +1,24 @@
+<?php
+include "../conexion.php";
+
+$sql = "SELECT
+    lbgn.genero_id,
+    COUNT(lbgn.genero_id) AS NumLibros
+FROM
+    libro AS lb
+    INNER JOIN libro_has_genero AS lbgn ON lbgn.libro_id = lb.id
+    INNER JOIN genero AS gn ON lbgn.genero_id = gn.id
+    INNER JOIN usuario_has_coleccion AS uscl ON uscl.libro_id = lb.id
+WHERE
+    uscl.coleccion_id = 2
+GROUP BY
+    lbgn.genero_id
+LIMIT 5;";
+
+$query = $conexion->query($sql);
+
+while ($resultado = $query->fetch_assoc()) {
+    echo json_encode($resultado);
+}
+    
+?>
