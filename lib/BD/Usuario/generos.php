68ea62002e45f25a59a1d4677adaac1987573b82
Umbrum <hakutoishiwashi@gmail.com>
2021-07-08 23:23:39 -0500

    Parche feo en para que tenga el formato adecuado

--- lib/BD/Usuario/generos.php
+++ lib/BD/Usuario/generos.php
@@ -11,12 +11,29 @@ include "../conexion.php";
     ";
     // echo $sql;
 
-    $query = $conexion->query($sql);
+    // $query = $conexion->query($sql);
     
-    while ($resultado = $query->fetch_assoc()) {
-        echo json_encode($resultado);
-    }
+    // while ($resultado = $query->fetch_all(PE)) {
+    //     echo json_encode($resultado);
+    // }
+    // $resultado = $query->fetch_all(PE);
+    
+    // echo json_encode($resultado);
+    
+    $db_name = "id17151113_myreview";
+    $db_server = "localhost";
+    $db_user = "id17151113_myreviewus";
+    $db_pass = "D+Gi3+{[/+sg!>Kk";
+
+    $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($result);
 
     mysqli_close($conexion);
-
 ?>
