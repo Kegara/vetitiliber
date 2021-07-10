7106c59e9afd4b91444250f0241e72b16c7f79e8
Paulo Romero <player_one_117@outlook.com>
2021-07-09 22:10:25 -0500

    PDF lector preparaciones

---
+++ lib/libro/leerPdf.dart
@@ -0,0 +1,30 @@
+
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+
+// ignore: camel_case_types
+class LeerPDF extends StatefulWidget {
+    static String id = 'leerPDF';
+    final String title;
+    LeerPDF({Key key, this.title}) : super(key: key);
+    _leerPDF createState() => _leerPDF();
+
+}
+
+// ignore: camel_case_types
+class _leerPDF extends State<LeerPDF> {
+  File file;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('por que solo hay videos en indio???'),
+      ),
+    );
+  }
+  
+}
