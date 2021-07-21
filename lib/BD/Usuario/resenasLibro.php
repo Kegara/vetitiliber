a0606c1f1f6d5d27eca0cbf5f7d42f3e1fb2640a
Umbrum <hakutoishiwashi@gmail.com>
2021-07-21 16:36:39 -0500

    Consulta para buscar las reseñas de un libro

---
+++ lib/BD/Usuario/resenasLibro.php
@@ -0,0 +1,35 @@
+<?php
+
+include "../conexion.php";
+
+if (isset($_POST)){
+    $id = $_POST['id'];
+    $sql = 
+    "SELECT
+      resena.id,
+      resena.calificacion,
+      resena.contenido,
+      resena.usuario_id
+    FROM
+      resena
+    WHERE
+      resena.libro_id = $id;";
+
+    $db_name = "id17151113_myreview";
+    $db_server = "localhost";
+    $db_user = "id17151113_myreviewus";
+    $db_pass = "D+Gi3+{[/+sg!>Kk";
+
+    $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($result);
+}
+
+    mysqli_close($conexion);
+?>
