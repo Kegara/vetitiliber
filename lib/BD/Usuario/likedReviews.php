1ea29bb7ef1c900f38e405c0b6db8563b84b7bad
Umbrum <hakutoishiwashi@gmail.com>
2021-07-21 17:37:19 -0500

    Consulta para conseguir las reseñas que le gustan al usuario

---
+++ lib/BD/Usuario/likedReviews.php
@@ -0,0 +1,33 @@
+<?php
+
+include "../conexion.php";
+
+if (isset($_POST)){
+    $id = $_POST['id'];
+    $sql = 
+    "SELECT
+        resena_id AS id
+    FROM
+        reaccion
+    WHERE
+        reaccion.like = 1
+        AND reaccion.usuario_id = $id;";
+  
+    $db_name = "id17151113_myreview";
+    $db_server = "localhost";
+    $db_user = "id17151113_myreviewus";
+    $db_pass = "D+Gi3+{[/+sg!>Kk";
+
+    $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($result);
+}
+
+    mysqli_close($conexion);
+?>
