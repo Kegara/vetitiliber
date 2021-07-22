0961379deb80baf971bfa1b542d981088db312c8
Umbrum <hakutoishiwashi@gmail.com>
2021-07-22 16:04:11 -0500

    Cambio en formato de respuesta

--- lib/BD/Usuario/infoUsuario.php
+++ lib/BD/Usuario/infoUsuario.php
@@ -69,22 +69,13 @@ if (isset($_POST)){
         usuario AS us
     WHERE
         us.id = $id;";
-    
-    $db_name = "id17151113_myreview";
-    $db_server = "localhost";
-    $db_user = "id17151113_myreviewus";
-    $db_pass = "D+Gi3+{[/+sg!>Kk";
-
-    $db = new PDO("mysql:host={$db_server};dbname={$db_name};charset=utf8", $db_user, $db_pass);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    echo json_encode($result);
-}
+  
+    $query = $conexion -> query($sql);
+        
+    while ($resultado = $query->fetch_assoc()) {
+        echo json_encode($resultado);
+    }
 
     mysqli_close($conexion);
+}
 ?>
