5e6d16e10def6c0ebb6740d034281256f8a944a7
Umbrum <hakutoishiwashi@gmail.com>
2021-07-27 00:41:12 -0500

    Actualizacion de consulta para que tambien inserte configuracion

--- lib/BD/Usuario/registro.php
+++ lib/BD/Usuario/registro.php
@@ -8,8 +8,14 @@
         $email = $_POST["correoElectronico"];
 
 
-        $sql = "INSERT INTO usuario (nombre, contrasena, correoElectronico) VALUES ('$usuario','$contraseña','$email')";
+        $sql = "INSERT INTO usuario (nombre, contrasena, correoElectronico) VALUES ('$usuario','$contraseña','$email');";
         
+        mysqli_query($conexion,$sql);
+        
+        $id = mysqli_insert_id($conexion);
+        
+        $sql = "INSERT INTO configuracion (vLibros, vResenas, vPerfil, usuario_id) VALUES (0 ,0 ,0 ,$id)";
+
         mysqli_query($conexion,$sql);
 
         mysqli_close($conexion);
